a6f8f3ae78565e69bf0d8252f964ffe18337e808
Nambardar212 <nikhilsaini0210@gmail.com>
2022-10-20 13:54:42 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
+
 function App() {
+  const [txt, setShow] = useState('')
+
+  const handleClick = () => {
+        // console.log('djgfdj')
+        setShow("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+       <p id="para">{txt}</p>
+       <button id="click" type="submit" onClick={()=>{handleClick()}}>Click</button>
     </div>
   );
 }
